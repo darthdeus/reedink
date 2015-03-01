e75444ba36d5f4f09e0373a0080bf6ea30d81587
Jakub Arnold <darthdeus@gmail.com>
2015-03-01 21:03:30 +0100

    Extract readable/reading query into Models.Readable

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -1,17 +1,12 @@
 module Handler.Home where
 
-import Control.Lens hiding (from, on)
 import Import hiding (on, (==.))
-import Database.Esqueleto hiding ((^.))
-import qualified Database.Esqueleto as E
+import Control.Lens
+import Models.Readable
 
 getProfileR :: Handler Html
 getProfileR = do
   userId <- requireAuthId
-
-  items <- runDB $ select $ from $ \(r `LeftOuterJoin` u) -> do
-             on (r E.^. ReadableId ==. u E.^. UserReadingReadableId)
-             where_ (u E.^. UserReadingUserId ==. val userId)
-             return (r, u)
+  items <- runDB $ userReadableReadings userId
 
   defaultLayout $(widgetFile "profile")
