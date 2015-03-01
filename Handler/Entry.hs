20d937787d4914a7e022a467bb71c94b1faf366b
Zuzana Dostalova <zuzana.dostalova@icloud.com>
2015-03-02 00:06:12 +0100

    Entry index and new

---
+++ Handler/Entry.hs
@@ -0,0 +1,32 @@
+module Handler.Entry where
+
+import Import
+import Yesod.Form.Bootstrap3
+import Helpers.Common
+import Control.Lens
+
+getEntriesR :: Handler Html
+getEntriesR = do
+  userId <- requireAuthId
+  (form, _) <- generateFormPost $ entryForm userId Nothing
+  defaultLayout $(widgetFile "entries")
+
+
+postEntriesR :: Handler Html
+postEntriesR = do
+  userId <- requireAuthId
+  ((res, form), _) <- runFormPost $ entryForm userId Nothing
+
+  case res of
+    FormSuccess entry -> do
+      void . runDB $ insert entry
+      setMessage "New knowledge has been logged."
+      redirect EntriesR
+
+entryForm :: UserId -> Maybe Entry -> Form Entry
+entryForm userId mentry = renderBootstrap3 BootstrapBasicForm $ Entry
+  <$> areq textField (bs "Title") (mentry ^? _Just.entryTitle)
+  <*> aopt textField (bs "Description") (mentry ^? _Just.entryDescription)
+  <*> pure (maybe userId id (_entryUserId <$> mentry))
+  <*> lift (liftIO getCurrentTime)
+  <*  bootstrapSubmit (BootstrapSubmit ("Submit" :: Text) "btn-default" [])
