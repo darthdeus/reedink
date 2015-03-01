f650652f28373412acba6590af94bd0d55879309
Jakub Arnold <darthdeus@gmail.com>
2015-03-01 09:45:50 +0100

    Run configure before build

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,6 +41,7 @@ namespace :deploy do
   after :updated, :cabal_build do
     on roles(:app), in: :sequence, wait: 5 do
       within current_path do
+        execute :cabal, "configure"
         execute :cabal, "build"
       end
     end
