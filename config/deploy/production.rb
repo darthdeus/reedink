01bc7b20e93181b4a83b9565ea233ba4e3995c22
Jakub Arnold <darthdeus@gmail.com>
2015-03-01 08:48:50 +0100

    Capistrano config for production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{deploy@reedink.jakubarnold.cz}
+role :web, %w{deploy@reedink.jakubarnold.cz}
+role :db,  %w{deploy@reedink.jakubarnold.cz}
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w{deploy@example.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'reedink.jakubarnold.cz', user: 'deploy', roles: %w{web app} # , my_property: :my_value
 
 
 # Custom SSH Options
