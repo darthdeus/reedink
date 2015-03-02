d256c4926b1442d15c07672cd0a3e8bc1cd87bf8
Zuzana Dostalova <zuzana.dostalova@icloud.com>
2015-03-02 21:11:12 +0100

    Learning entries to the profile

--- Handler/Entry.hs
+++ Handler/Entry.hs
@@ -23,6 +23,20 @@ postEntriesR = do
       void . runDB $ insert entry
       setMessage "New knowledge has been logged."
       redirect EntriesR
+    _ -> do
+      setMessage "Something went wrong"
+      redirect EntriesR
+
+deleteEntryR :: EntryId -> Handler Html
+deleteEntryR entryId = do
+  userId <- requireAuthId
+  entry <- runDB $ get404 entryId
+
+  runDB $ do
+    delete entryId
+
+  setMessage "Deleted successful."
+  redirect EntriesR
 
 entryForm :: UserId -> Maybe Entry -> Form Entry
 entryForm userId mentry = renderBootstrap3 BootstrapBasicForm $ Entry
--- Handler/Home.hs
+++ Handler/Home.hs
@@ -1,12 +1,14 @@
 module Handler.Home where
 
-import Import hiding (on, (==.))
+import Import hiding (on)
 import Control.Lens
+import Helpers.Common
 import Models.Readable
 
 getProfileR :: Handler Html
 getProfileR = do
   userId <- requireAuthId
   items <- runDB $ userReadableReadings userId
+  entries <- runDB $ selectList [EntryUserId ==. userId] []
 
   defaultLayout $(widgetFile "profile")
