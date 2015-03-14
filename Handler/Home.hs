2911b8ed4c4c8864979a98a0f642b00ef7abc90d
Jakub Arnold <darthdeus@gmail.com>
2015-03-14 23:24:24 +0100

    Show skill progress for a given day if there was one

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -1,10 +1,12 @@
 module Handler.Home where
 
-import Import hiding (on)
-import Control.Lens
+import Import hiding (on, (==.))
+import qualified Import as I
 import Helpers.Common
 import Models.Readable
 import Yesod.Form.Bootstrap3
+import Database.Esqueleto
+import qualified Database.Esqueleto as E
 
 skillForm :: UserId -> Maybe Skill -> Form Skill
 skillForm userId ms = renderBootstrap3 BootstrapBasicForm $ Skill
@@ -16,14 +18,20 @@ getProfileR :: Handler Html
 getProfileR = do
   userId <- requireAuthId
   items <- runDB $ userReadableReadings userId
-  entries <- runDB $ selectList [EntryUserId ==. userId] []
+  entries <- runDB $ selectList [EntryUserId I.==. userId] []
 
   defaultLayout $(widgetFile "profile")
 
 getSkillsR :: Handler Html
 getSkillsR = do
   userId <- requireAuthId
-  skills <- runDB $ selectList [SkillUserId ==. userId] []
+  day <- fmap utctDay $ liftIO getCurrentTime
+
+  items <- runDB $ select $ from $ \(s `LeftOuterJoin` mp) -> do
+    on ((just (s E.^. SkillId) ==. mp ?. ProgressSkillId) &&.
+        (mp ?. ProgressCreatedAt ==. just (val day)))
+    where_ (s E.^. SkillUserId ==. val userId)
+    return (s, mp)
 
   (form, _) <- generateFormPost $ skillForm userId Nothing
 
@@ -48,17 +56,18 @@ deleteSkillR :: SkillId -> Handler Html
 deleteSkillR key = do
   -- TODO - only delete skills that belong to the currently logged in user
   -- TODO - delete all Progress that belongs to a given skill
-  runDB $ delete key
+  runDB $ I.delete key
   setMessage "Skill deleted"
   redirect SkillsR
 
 postProgressR :: SkillId -> Handler Html
 postProgressR key = do
   userId <- requireAuthId
+
   day <- fmap utctDay $ liftIO getCurrentTime
-  mprogress <- runDB $ selectFirst [ProgressSkillId ==. key,
-                                    ProgressUserId ==. userId,
-                                    ProgressCreatedAt ==. day] []
+  mprogress <- runDB $ selectFirst [ProgressSkillId I.==. key,
+                                    ProgressUserId I.==. userId,
+                                    ProgressCreatedAt I.==. day] []
 
   case mprogress of
     Just _ ->
